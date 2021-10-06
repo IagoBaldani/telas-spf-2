eb4db0d1f996e7d825fc395a1804b08972980fa8
Iago Baldani <iagobaldanialmeida@hotmail.com>
2021-10-06 15:34:36 -0300

    Conclusão das páginas: Dados do programa

--- src/views/dados-programa-busca/App.vue
+++ src/views/dados-programa-busca/App.vue
@@ -1,5 +1,74 @@
 <template>
-  <Header/>
+    <Header/>
+    <main>
+        <div class="container-fluid">
+            <div class="row justify-content-evenly mt-4 mb-3">
+                <div class="titulo col-xl-4">
+                    <h3 class="fw-bold">Busca por Formação:</h3>
+                </div>
+                <div class="col-xl-7 d-flex justify-content-center"></div>
+            </div>
+            <div class="row justify-content-evenly" id="programas">
+                <div class="col-xl-4">
+                    <form>
+                        <div class="mb-3">
+                            <input name="nome" type="text" class="form-control mt-4" placeholder="Nome"
+                                   id="filtro-nome">
+                        </div>
+                        <div class="mb-3">
+                            <select class="form-select mt-4" id="filtro-status">
+                                <option disabled selected value="0">Status</option>
+                                <option value="1">Em andamento</option>
+                                <option value="2">Encerrado</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-xl-7">
+                    <div class="aviso">
+                        <h4 class="titulo fw-bold"> Não foi encontrado nenhum resultado com os parâmetros
+                            informados </h4>
+                        <button class="mt-3 form-control recarregar" @click="recarregaLista"> RECARREGAR LISTA
+                        </button>
+                    </div>
+                    <div class="table-wrapper-scroll-y my-custom-scrollbar">
+                        <table class="table table-bordered tabela mt-4 " id="tab">
+                            <tbody align="center">
+                            <tr class="programa" v-for="programa in programas" v-bind:key="programa">
+                                <th scope="row" width="50">{{ programa.id }}</th>
+                                <td id="info-nome"> {{ programa.nome }}</td>
+                                <td>{{ programa.turma }}</td>
+                                <td id="info-status" class="info-status"
+                                    v-bind:class="(programa.status == 'Encerrado')?'encerrado':'em-andamento'">
+                                    {{ programa.status }}
+                                </td>
+                                <td class="imagem rounded" width="50">
+                                    <a href="/dados-programa-cadastro_edicao">
+                                        <img src="@/assets/imgs/settings_white_24dp.svg"/>
+                                    </a>
+                                </td>
+                                <td class="imagem-coluna rounded" width="50">
+                                    <a href="/dados-programa-visualizacao">
+                                        <img src="@/assets/imgs/school_white_24dp.svg">
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-evenly">
+                <div class="col-xl-4">
+                    <input value="BUSCAR" type="button" class="mt-5 form-control submit" @click="filtraDados"/>
+                </div>
+                <div class="col-xl-7 justify-content-end d-flex">
+                    <a href="/dados-programa-cadastro_edicao"
+                       class="mt-5 form-control cadastro d-flex justify-content-center"> NOVO CADASTRO </a>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -9,20 +78,254 @@ export default {
   name: 'App',
   components: {
     Header
+  },
+  data () {
+    return {
+      programas: [
+        {
+          id: 1,
+          nome: 'Java',
+          turma: 'Turma I 2020',
+          status: 'Encerrado'
+        },
+        {
+          id: 2,
+          nome: 'BI',
+          turma: 'Turma I 2021',
+          status: 'Em andamento'
+        },
+        {
+          id: 3,
+          nome: 'Mainframe',
+          turma: 'Turma I 2021',
+          status: 'Em andamento'
+        }
+      ]
+    }
+  },
+  methods: {
+    filtraDados () {
+      let dadosLinhas = this.pegaDados()
+
+      let nomeProcurado = document.querySelector('#filtro-nome').value
+      let statusProcurado = document.querySelector('#filtro-status').value
+
+      let linhasNl = document.querySelectorAll('.programa')
+      var linhas = Array.prototype.slice.call(linhasNl)
+
+      const arrayBoolLinhas = this.verifica(dadosLinhas, nomeProcurado, statusProcurado)
+      this.mudaVisibilidade(arrayBoolLinhas, linhas)
+    },
+
+    pegaDados () {
+      let linhas = document.querySelectorAll('.programa')
+      let arrayDadosDasLinhas = []
+
+      linhas.forEach(linha => {
+        let dadosLinha = []
+        let nome = linha.querySelector('#info-nome').textContent
+        let status = this.trataStatus(linha)
+
+        dadosLinha.push(nome, status)
+        arrayDadosDasLinhas.push(dadosLinha)
+      })
+
+      return arrayDadosDasLinhas
+    },
+
+    trataStatus (item) {
+      let statusTxt = item.querySelector('#info-status').textContent
+      let status = 0
+      if (statusTxt === 'Em andamento') {
+        status = 1
+        return status
+      } else if (statusTxt === 'Encerrado') {
+        status = 2
+        return status
+      }
+
+      return status
+    },
+
+    verifica (dadosLinhas, nomeProcurado, statusProcurado) {
+      let arrayBoolLinhas = []
+      let expressao = new RegExp(nomeProcurado, 'i')
+
+      dadosLinhas.forEach(dadosLinha => {
+        const boolLinha = []
+        console.log(dadosLinha)
+        if (expressao.test(dadosLinha[0]) || nomeProcurado == '') {
+          boolLinha.push(true)
+        } else {
+          boolLinha.push(false)
+        }
+
+        if (statusProcurado == dadosLinha[1] || statusProcurado == 0) {
+          boolLinha.push(true)
+        } else {
+          boolLinha.push(false)
+        }
+
+        arrayBoolLinhas.push(boolLinha)
+      })
+
+      return arrayBoolLinhas
+    },
+
+    mudaVisibilidade (arrayBoolLinhas, linhas) {
+      let i
+      var contador = 0
+      let aviso = document.querySelector('.aviso')
+      var qtdLinhas = linhas.length
+
+      for (i = 0; i < linhas.length; i++) {
+        if (arrayBoolLinhas[i][0] && arrayBoolLinhas[i][1]) {
+          linhas[i].style.display = ''
+        } else {
+          linhas[i].style.display = 'none'
+          contador++
+        }
+      }
+
+      if (qtdLinhas === contador) {
+        aviso.style.display = 'flex'
+      } else {
+        aviso.style.display = 'none'
+      }
+    },
+
+    recarregaLista () {
+      let linhas = document.querySelectorAll('.programa')
+      let aviso = document.querySelector('.aviso')
+
+      linhas.forEach(linha => {
+        linha.style.display = ''
+      })
+
+      aviso.style.display = 'none'
+    }
   }
 }
 </script>
 
 <style>
-*{
-    box-sizing: border-box;
-    font-family: 'Montserrat', sans-serif;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-}
-
-body{
+body {
     background-color: #EBEBEB !important;
 }
+a{
+    text-decoration: none !important;
+}
+
+.tabela{
+    border: 1px solid #BCB3B3 !important;
+    background-color: white;
+}
+
+.titulo{
+    color: #090B2E;
+}
+
+.botao{
+    background-color: #AB0045 !important;
+    border-style: none !important;
+    width: 26.9em !important;
+    border-radius: 2px !important;
+}
+
+.campo{
+    border-radius: 2px !important;
+    background-color: white !important;
+    color: #737373 !important;
+}
+
+.th-id{
+    width: 2em !important;
+}
+
+.th-ms{
+    width: 15em !important;
+}
+
+.td-button{
+    width: 1.5em !important;
+    background-color: #FFB700 !important;
+}
+
+.nome-participante{
+    color: #737373;
+}
+.campo-tabela{
+    color: #737373 !important;
+}
+
+.submit, .cadastro, .recarregar{
+    color: white !important;
+    font-weight: bold !important;
+    border-radius: 5px !important;
+}
+
+.submit{
+    background-color: #AB0045 !important;
+}
+
+.cadastro{
+    background-color: #FFB700 !important;
+    max-width: 575px !important;
+}
+
+.em-andamento{
+    color: green;
+    font-weight: bold;
+}
+
+.encerrado{
+    color: darkred;
+    font-weight: bold;
+}
+
+.imagem{
+    background-color:  #AB0045 !important;
+}
+
+.imagem-coluna{
+    background-color: #FFB700 !important;
+}
+
+.my-custom-scrollbar {
+    position: relative;
+    height: 59vh;
+    overflow: auto;
+}
+
+.table-wrapper-scroll-y {
+    display: block;
+    height: 59vh;
+}
+
+.invisivel{
+    display: none;
+}
+
+.aviso{
+    display: none;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2em;
+    text-align: center;
+    position: absolute;
+    z-index: 100;
+}
+
+.recarregar{
+    background-color: #090B2E !important;
+    max-width: 50%;
+    cursor: pointer !important;
+    transition: all linear 0.3s !important;
+}
+
+.recarregar:hover{
+    background-color: #141863 !important;
+}
+
 </style>
--- src/views/dados-programa-cadastro_edicao/App.vue
+++ src/views/dados-programa-cadastro_edicao/App.vue
@@ -1,5 +1,136 @@
 <template>
-  <Header/>
+    <Header/>
+    <main>
+        <div class="container-fluid">
+            <div class="row mt-4 justify-content-evenly">
+                <div class="div-titulo col-xl-4">
+                    <h3 class="fw-bold titulo">Dados da Formação:</h3>
+                </div>
+                <div class="col-xl-4"></div>
+                <div class="col-xl-2"></div>
+            </div>
+            <div class="row d-flex justify-content-evenly">
+                <div class="col-xl-4">
+                    <div class="mb-3">
+                        <label for="nome" class="form-label fw-bold mb-0 titulo">Nome</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="nome"
+                            required
+                            placeholder="Java"
+                            v-model="modelNome"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="inicio" class="form-label fw-bold mb-0 titulo">Início do Programa</label>
+                        <input
+                            type="date"
+                            class="form-control"
+                            id="inicio"
+                            required
+                            placeholder="dd/MM/yyyy"
+                            v-model="modelInicio"
+                        />
+                    </div>
+                    <div class="mb-3">
+                        <label for="termino" class="form-label fw-bold mb-0 titulo">Término do Programa</label>
+                        <input
+                            type="date"
+                            class="form-control"
+                            id="termino"
+                            required
+                            placeholder="dd/MM/yyyy"
+                            v-model="modelTermino"
+                        />
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label class="form-label fw-bold mb-0 titulo">Coordenador</label>
+                        <select class="form-select" id="coordenadores" v-model="modelCoordenador">
+                            <option
+                                id="coordenador"
+                                v-for="coordenador in coordenadores"
+                                v-bind:value="coordenador.nome"
+                                v-bind:key="coordenador"
+                            >
+                                {{ coordenador.nome }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="turma" class="form-label fw-bold mb-0 titulo">Turma</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="turma"
+                            required
+                            placeholder="2021.1"
+                            v-model="modelTurma"
+                        />
+                    </div>
+                    <div class="erro"> Todos os campos são obrigatórios </div>
+                </div>
+                <div class="col-xl-4"></div>
+                <div class="col-xl-2"></div>
+                </div>
+                <div class="row d-flex justify-content-evenly">
+                    <div class="col-xl-4">
+                        <input
+                            value="CONFIRMAR"
+                            type="button"
+                            name="botao-ok"
+                            class="mt-5 form-control submit"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                            @click.prevent="enviarDados"/>
+                    </div>
+                    <div class="col-xl-4"></div>
+                    <div class="col-xl-2"></div>
+                </div>
+        </div>
+    </main>
+
+    <!-- Modal   -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-xl modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header border-0">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body d-flex justify-content-between">
+                    <div>
+                        <h1 class="modal-title form-label fw-bold mb-0 titulo">
+                            Deseja confirmar os seguintes dados?
+                        </h1>
+                    </div>
+                    <div class="conteudomodal d-flex flex-column justify-content-center mb-0">
+                        <div class="mt-3">
+                            <ul class="fw-bold subtitulo text-start">
+                                Informações gerais:
+                                <li>Nome: {{ programa.nome }}</li>
+                                <li>Início do Programa: {{ programa.inicio }}</li>
+                                <li>Término do Programa: {{ programa.termino }}</li>
+                                <li>Instrutor: {{ programa.coordenador }}</li>
+                                <li>Turma: {{ programa.turma }}</li>
+                            </ul>
+                        </div>
+                        <div class="mt-3 modal-footer border-0 justify-content-around">
+                            <div>
+                                <button type="button" class="btn submit-modal">
+                                    CONFIRMAR
+                                </button>
+                            </div>
+                            <div>
+                                <button type="button" class="btn cancel-modal" data-bs-dismiss="modal">
+                                    CANCELAR
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,20 +140,151 @@ export default {
   name: 'App',
   components: {
     Header
+  },
+  data () {
+    return {
+      coordenadores: [
+        {
+          id: 1,
+          nome: 'Kaiqui Lopes'
+        },
+        {
+          id: 2,
+          nome: 'Luciana Neuber'
+        }
+      ],
+      programa: {
+        nome: '',
+        inicio: '',
+        termino: '',
+        coordenador: '',
+        turma: ''
+      }
+    }
+  },
+  methods: {
+    enviarDados () {
+      this.programa.nome = this.modelNome
+      this.programa.inicio = formataDataParaExibicao(this.modelInicio)
+      this.programa.termino = formataDataParaExibicao(this.modelTermino)
+      this.programa.coordenador = this.modelCoordenador
+      this.programa.turma = this.modelTurma
+    }
   }
 }
+
+function formataDataParaExibicao (data) {
+  const dataPreForm = new Date(data)
+  const dataFormatada = `${dataPreForm.getUTCDate()}/${dataPreForm.getUTCMonth() + 1}/${dataPreForm.getUTCFullYear()}`
+
+  return dataFormatada
+}
+
 </script>
 
 <style>
-*{
-    box-sizing: border-box;
-    font-family: 'Montserrat', sans-serif;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
+body {
+    background-color: #ebebeb !important;
 }
 
-body{
-    background-color: #EBEBEB !important;
+.titulo {
+    color: #090B2E;
 }
+
+.subtitulo {
+    color: #737373;
+}
+
+#disabledTextInput {
+    background-color: #D3CACA;
+    border: 1px solid #BCB3B3;
+}
+
+#formulario-principal {
+    text-align: left;
+}
+
+textarea {
+    resize: none !important;
+}
+
+.cadastro {
+    background-color: #FFB700 !important;
+    color: white !important;
+    font-weight: bold !important;
+    border-radius: 5px !important;
+    max-width: 575px !important;
+
+}
+
+.status-encerrado, .erro{
+    color: darkred;
+    font-weight: bold;
+}
+
+.status-andamento {
+    color: green;
+    font-weight: bold;
+}
+
+.imagem {
+    background-color: #AB0045 !important;
+}
+
+.imagem-coluna {
+    background-color: #FFB700 !important;
+}
+
+.my-custom-scrollbar {
+    position: relative;
+    height: 59vh;
+    overflow: auto;
+}
+
+.table-wrapper-scroll-y {
+    display: block;
+    height: 59vh;
+}
+
+.modal-body, .modal-header, .modal-footer {
+    text-align: center;
+    background-color: #EBEBEB
+}
+
+.modal-body {
+    min-height: 55vh;
+    flex-direction: column;
+}
+
+.submit-modal, .cancel-modal {
+    color: white !important;
+    font-weight: bold !important;
+    border-radius: 5px !important;
+    width: 350px;
+    font-size: 25px !important;
+}
+
+.submit, .submit-modal {
+    color: white !important;
+    font-weight: bold !important;
+    background-color: #AB0045 !important;
+}
+
+.cancel, .cancel-modal {
+    color: white !important;
+    font-weight: bold !important;
+    background-color: #FFB700 !important;
+}
+
+.conteudomodal {
+    display: flex;
+    justify-content: center;
+    min-height: 40vh;
+    font-size: 21px;
+}
+
+.modal-content {
+    height: 80vh !important;
+}
+
 </style>
